43fe526bcc84a4de254a2c3b7e5018f4e99b6d0c
cbolles <collinbolles@gmail.com>
2024-06-12 14:04:31 -0400

    Getting started with scholarly analysis including mypy

---
+++ packages/frp/frp/scholarly.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import pandas as pd
+
+
+class FRPScholarlyAnalysis:
+    """
+    Handles the cleaning and matching of MyCV CSVs to FRPs
+    """
+    def load(self, csv_location: Path) -> pd.DataFrame:
+        pass
