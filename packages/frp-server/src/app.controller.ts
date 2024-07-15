2b4ff41a0b2b54e0d6fd018c5999c334facc3c9b
cbolles <collinbolles@gmail.com>
2024-07-15 16:41:44 -0400

    Create job

--- packages/frp-server/src/app.controller.ts
+++ packages/frp-server/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { KubeConfig, BatchV1Api } from '@kubernetes/client-node';
+import { KubeConfig, BatchV1Api, V1Job, V1ObjectMeta, V1JobSpec, V1PodSpec, V1PodTemplateSpec } from '@kubernetes/client-node';
 
 @Controller()
 export class AppController {
@@ -11,10 +11,38 @@ export class AppController {
     const kc = new KubeConfig();
     kc.loadFromDefault();
 
-    const batchV1beta1Api = kc.makeApiClient(BatchV1Api);
-    const jobs = await batchV1beta1Api.listNamespacedJob('sail-24887a');
+    const metadata = new V1ObjectMeta();
+    metadata.name = 'pi';
 
-    console.log(jobs.body);
+    const job = new V1Job();
+    job.metadata = metadata;
+    job.apiVersion = 'batch/v1';
+
+    job.kind = 'Job';
+    job.spec = new V1JobSpec();
+    job.spec.template = new V1PodTemplateSpec();
+
+    console.log(job.spec);
+
+    job.spec.template.spec = new V1PodSpec();
+    job.spec.template.spec.containers = [];
+
+    job.spec!.template.spec!.containers.push({
+      name: 'pi',
+      image: 'perl:5.34.0',
+      command: ["perl",  "-Mbignum=bpi", "-wle", "print bpi(2000)"]
+    });
+    job.spec!.template.spec!.restartPolicy = 'Never';
+
+    console.log(job);
+
+    const batchV1Api = kc.makeApiClient(BatchV1Api);
+    try {
+      const createJobRes = await batchV1Api.createNamespacedJob('sail-24887a', job);
+      console.log(createJobRes.body);
+    } catch (e) {
+      console.log(e);
+    }
 
     return this.appService.getHello();
   }
