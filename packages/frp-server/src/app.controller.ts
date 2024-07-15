a39c0a7354f080fde3b0378e93f8826142be3fe9
cbolles <collinbolles@gmail.com>
2024-07-15 15:21:15 -0400

    Attempt to list pods in namespace

--- packages/frp-server/src/app.controller.ts
+++ packages/frp-server/src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { KubeConfig, CoreV1Api } from '@kubernetes/client-node';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
+    const kc = new KubeConfig();
+    kc.loadFromDefault();
+
+    const k8sApi = kc.makeApiClient(CoreV1Api);
+    const podsRes = await k8sApi.listNamespacedPod('sail-24887a');
+    console.log(podsRes.body);
+
     return this.appService.getHello();
   }
 }
