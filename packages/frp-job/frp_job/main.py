ce81aa000dd27da2caad09a69658f16bb734b9c7
cbolles <collinbolles@gmail.com>
2024-07-17 14:51:32 -0400

    Add in logic for matching

--- packages/frp-job/frp_job/main.py
+++ packages/frp-job/frp_job/main.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from pathlib import Path
 import urllib.request
 from frp import FRPScholarlyAnalysis, Matcher
+import toml
 
 
 def download_csv(csv_url: str, download_location: Path):
@@ -12,38 +13,54 @@ def download_csv(csv_url: str, download_location: Path):
     urllib.request.urlretrieve(csv_url, download_location)
 
 
-def run_analysis(csv_location: Path, config_path: Path):
+def run_analysis(csv_location: Path, config_path: Path, frp_title: str, frp_year: int):
     """
     Handles passing the running the analysis process.
     """
+    # Read in the config
+    if not config_path.exists():
+        print(f'File {config_path} does not exist')
+    with open(config_path, 'r') as config_file:
+        config = toml.load(config_file)
+
+    # Configure the matcher
+    matcher = Matcher(config['scholarly']['matcher'])
+
+    # Configure the analyzer
+    analyzer = FRPScholarlyAnalysis(matcher, config['scholarly'])
+
+    # Collect the results
+    return analyzer.run_frp_analysis(csv_location, frp_title, frp_year)
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('csv_url',
-                        required=True,
                         help='URL of the CSV for publications to download')
     parser.add_argument('frp_title',
-                        required=True,
                         help='FRP title for matching against')
     parser.add_argument('frp_year',
-                        required=True,
                         help='Year the FRP should be matched against')
     parser.add_argument('--csv_location',
                         required=False,
-                        default='./publications.csv',
+                        default='./data/publications.csv',
                         help='Location to download the CSV to')
     parser.add_argument('--config_location',
                         required=False,
-                        default='./config.toml',
+                        default='./data/config.toml',
                         help='Location of the config for running the analysis')
 
     args = parser.parse_args()
 
     # Download the CSV to a local location
-    download_csv(args.csv_url, args.download_location)
+    download_csv(args.csv_url, args.csv_location)
 
     # Run the matching logic
+    results = run_analysis(args.csv_location,
+                           args.config_location,
+                           args.frp_title,
+                           args.frp_year)
+    print(results)
 
 if __name__ == '__main__':
     main()
