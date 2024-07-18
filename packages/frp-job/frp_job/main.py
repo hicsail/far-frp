8b099758dbad9e5d291c5ce3e75069f925c43ec9
cbolles <collinbolles@gmail.com>
2024-07-18 10:12:21 -0400

    Add basic skeleton for calling webhook

--- packages/frp-job/frp_job/main.py
+++ packages/frp-job/frp_job/main.py
@@ -3,8 +3,10 @@ from pathlib import Path
 from frp import FRPScholarlyAnalysis, Matcher
 import toml
 import requests
+import pandas as pd
 
-def download_csv(csv_url: str, download_location: Path):
+
+def download_csv(csv_url: str, download_location: Path) -> None:
     """
     Handles downloading the CSV from the given
     URL.
@@ -15,7 +17,7 @@ def download_csv(csv_url: str, download_location: Path):
         csv_file.write(result.content)
 
 
-def run_analysis(csv_location: Path, config_path: Path, frp_title: str, frp_year: int):
+def run_analysis(csv_location: Path, config_path: Path, frp_title: str, frp_year: int) -> pd.DataFrame:
     """
     Handles passing the running the analysis process.
     """
@@ -35,6 +37,14 @@ def run_analysis(csv_location: Path, config_path: Path, frp_title: str, frp_year
     return analyzer.run_frp_analysis(csv_location, frp_title, frp_year)
 
 
+def return_results(results: pd.DataFrame, webhook_url: str) -> None:
+    """
+    Return the results back to the specified location
+    """
+    payload = results.to_json()
+    requests.post(webhook_url, data=payload)
+
+
 def main():
     parser = ArgumentParser()
     parser.add_argument('csv_url',
@@ -43,6 +53,8 @@ def main():
                         help='FRP title for matching against')
     parser.add_argument('frp_year',
                         help='Year the FRP should be matched against')
+    parser.add_argument('webhook_url',
+                        help='The webhook to call when the matching has completed')
     parser.add_argument('--csv_location',
                         required=False,
                         default='./data/publications.csv',
@@ -62,7 +74,9 @@ def main():
                            Path(args.config_location),
                            args.frp_title,
                            int(args.frp_year))
-    print(results)
+
+    # Pass the results back to the webhook
+    return_results(results, args.webhook_url)
 
 if __name__ == '__main__':
     main()
