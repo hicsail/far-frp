a1f6d5dbaa0bf2abd812a3cc6de014a5d991e2ec
cbolles <collinbolles@gmail.com>
2024-08-05 13:34:59 -0400

    Setup grants upload handler

--- packages/frp-server/src/grants-upload/grants-upload.controller.ts
+++ packages/frp-server/src/grants-upload/grants-upload.controller.ts
@@ -1,12 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { GrantsUploadWebhookPayload } from '../nocodb/dto/grant-upload.dto';
 import { NocoDBInsertWebhookPayload } from '../nocodb/dto/webhook.dto';
+import { GrantsUploadService } from './grants-upload.service';
 
-@Controller('grants-upload')
+@Controller('grants')
 export class GrantsUploadController {
+
+  constructor(private readonly grantsUploadService: GrantsUploadService) {}
 
   @Post('nocodbWebhook')
   async handleNocodbWebhook(@Body() payload: NocoDBInsertWebhookPayload<GrantsUploadWebhookPayload>): Promise<void> {
+    const grantsUploadID = payload.data.rows[0].Id.toString();
+    const csvUrlStub = payload.data.rows[0].FAR[0].signedPath;
 
+    await this.grantsUploadService.handleUpload(grantsUploadID, csvUrlStub);
   }
 }
