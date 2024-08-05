10e7e4af2132807a639f133e770088c21db73b8d
cbolles <collinbolles@gmail.com>
2024-08-05 17:00:01 -0400

    Create standard way of converting columns

--- packages/frp/frp/analysis.py
+++ packages/frp/frp/analysis.py
@@ -2,11 +2,27 @@ from abc import ABC, abstractmethod
 from frp.matcher import Matcher
 from pathlib import Path
 import pandas as pd
+from dataclasses import dataclass
+
+
+@dataclass
+class ColumnConversion:
+    """
+    Represents the process for converting a standardizing
+    a column. Including keeping track of the different
+    names the column could have and what the output
+    name should be.
+    """
+    possible_names: list[str]
+    output_name: str
+    column_type: str
 
 
 class Analysis(ABC):
-    def __init__(self, matcher: Matcher, config: dict):
+    def __init__(self, matcher: Matcher, config: dict, columns: list[ColumnConversion]):
         self._matcher = matcher
+
+        self.columns = columns
 
         # Getting the mapping between columns and inputs
         # to the matcher
@@ -15,12 +31,33 @@ class Analysis(ABC):
         # ex) publication_title: 'Title OR Chapter title'
         self._mappings = config['matcher']['mappings']
 
+
     def _load(self, csv_location: Path) -> pd.DataFrame:
         """
         Read in the dataframe from the CSV. Does not additional
         processing
         """
         return pd.read_csv(csv_location)
+
+    def _convert_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        output = pd.DataFrame()
+
+        # First, convert the columns from their original names to a standard name.
+        # ie) If the column could appear as "Title" or "Title or Chapter Title",
+        # convert to one name like "Title"
+        for column in self.columns:
+            # Determine which of the possible column names is correct
+            try:
+                original_column_name = next(name for name in column.possible_names if name in df.columns)
+            except:
+                raise Exception(f'Could not find suitable column for {column.output_name}')
+            output[column.output_name] = df[original_column_name]
+
+        # Next, convert the columns to the appropriate types
+        column_conversion_dict = { column.output_name: column.column_type for column in self.columns }
+        output = output.astype(column_conversion_dict)
+
+        return output
 
     @abstractmethod
     def _standardize(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -64,6 +101,7 @@ class Analysis(ABC):
 
     def run_frp_analysis(self, csv_location: Path, frp_title: str, year: int) -> pd.DataFrame:
         df = self._load(csv_location)
+        df = self._convert_columns(df)
         df = self._standardize(df)
         df = self._filter(df, year)
         df = self._augment(df)
